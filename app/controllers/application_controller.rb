e44f483962b3b578d3cac9a98629e8a1bb8992dc
David Wenzel <dwenzel95@gmail.com>
2023-01-04 18:43:52 -0600

    commit tree 9db20ce9d31b0b5230c2da393082f9b01e66c294

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+
+    get '/bakeries' do
+      bakeries = Bakery.all
+      bakeries.to_json
+    end
+
+    get '/bakeries/:id' do
+      bakery = Bakery.find_by(params[:id])
+      
+      bakery.to_json
+      # bakery.to_json(include :baked_goods)
+      bakery.to_json(include: :baked_goods)
+    end
+
+    get '/baked_goods/by_price' do
+      price = BakedGood.all.order(price: :desc)
+      price.to_json
+    end
+
+    get '/baked_goods/most_expensive' do
+      price = BakedGood.all.order(price: :desc).first
+      price.to_json
+    end
+
+
 
 end
